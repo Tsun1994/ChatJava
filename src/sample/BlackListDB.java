2d4c7507746f7df65b058c7dedff67c7e3f24855
Alexandr <tank006@yandex.ru>
2021-04-12 18:16:59 +0400

    finish

--- src/sample/BlackListDB.java
+++ src/sample/BlackListDB.java
@@ -1,40 +1,40 @@
-//package sample;
-//
-//import java.sql.*;
-//
-//public class BlackListDB {
-//    private static Connection connection;
-//    private static Statement statement;
-//
-//    //проба пера подключения к базе данных черного списка, но не выходит сделать это в ConsoleServer
-//    public static void connect() {
-//        try {
-//            Class.forName("org.sqlite.JDBC");
-//            connection = DriverManager.getConnection("jdbc:sqlite:main.db");
-//            statement = connection.createStatement();
-//        } catch (ClassNotFoundException | SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public static String infoBlacklistBD ( String nickFrom) {
-//        String query = String.format("select nickTo from blacklist where nickFrom='%s'",  nickFrom);
-//        try {
-//            ResultSet rs = statement.executeQuery(query);
-//            if (rs.next()){
-//                rs.getString("nickTo");
-//            }
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//        return null;
-//    }
-//
-//    public static void disconnect() {
-//        try {
-//            connection.close();
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
+package sample;
+
+import java.sql.*;
+
+public class BlackListDB {
+    private static Connection connection;
+    private static Statement statement;
+
+    //проба пера подключения к базе данных черного списка, но не выходит сделать это в ConsoleServer
+    public static void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+            statement = connection.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String infoBlacklistBD ( String nickFrom) {
+        String query = String.format("select nickTo from blacklist where nickFrom='%s'",  nickFrom);
+        try {
+            ResultSet rs = statement.executeQuery(query);
+            if (rs.next()){
+                rs.getString("nickTo");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/server/ClientHandler.java
+++ src/server/ClientHandler.java
@@ -1,178 +1,178 @@
-//package server;
-//
-//import sample.BlackListDB;
-//
-//import java.io.DataInputStream;
-//import java.io.DataOutputStream;
-//import java.io.IOException;
-//import java.net.Socket;
-//import java.sql.DriverManager;
-//import java.sql.PreparedStatement;
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class ClientHandler {
-//
-//    private ConsoleServer server;
-//    private Socket socket;
-//    private DataOutputStream out;
-//    private DataInputStream in;
-//    private String nickname;
-//
-////    public void blacklistBD (String nickTo, String nickFrom){
-////        try {
-////            Class.forName("org.sqlite.JDBC");
-////
-////            connection = DriverManager.getConnection("jdbc:sqlite:main.db");
-////            statement = connection.createStatement();
-////        } catch (ClassNotFoundException | SQLException e) {
-////            e.printStackTrace();
-////        }
-////    }
-////        String query = String.format("select '%s' from blacklist where nickFrom='%s'", nickTo, nickFrom);
-////        try {
-////            ResultSet rs = statement.executeQuery(query); // возвращает выборку через select
-////            int myHash = pass.hashCode();
-////
-////            if (rs.next()) {
-////                String nick = rs.getString(1);
-////                int dbHash = rs.getInt(2);
-////                if (myHash == dbHash) {
-////                    return nick;
-////                }
-////            }
-////        } catch (SQLException e) {
-////            e.printStackTrace();
-////        }
-////    }
-//
-//    // черный список у пользователя, а не у сервера
-//    List<String> blackList;
-//
-//    public ClientHandler(ConsoleServer server, Socket socket) {
+package server;
+
+import sample.BlackListDB;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientHandler {
+
+    private ConsoleServer server;
+    private Socket socket;
+    private DataOutputStream out;
+    private DataInputStream in;
+    private String nickname;
+
+//    public void blacklistBD (String nickTo, String nickFrom){
 //        try {
-//            this.server = server;
-//            this.socket = socket;
-//            this.in = new DataInputStream(socket.getInputStream());
-//            this.out = new DataOutputStream(socket.getOutputStream());
-//            this.blackList = new ArrayList<>();
+//            Class.forName("org.sqlite.JDBC");
 //
-//            new Thread(() -> {
-//                boolean isExit = false;
-//                try {
-//                    while (true) {
-//                        String str = in.readUTF();
-//                        if (str.startsWith("/auth")){
-//                            String[] tokens = str.split(" ");
-//                            String nick = AuthService.getNicknameByLoginAndPass(tokens[1], tokens[2]);
-//                            if (nick != null) {
-//                                if (!server.isNickBusy(nick)) {
-//                                    sendMsg("/auth-OK");
-//                                    setNickname(nick);
-//                                    server.subscribe(ClientHandler.this);
-//                                    break;
-//                                } else {
-//                                    sendMsg("Учетная запись уже используется");
-//                                }
-//                            } else {
-//                                sendMsg("Неверный логин/пароль");
-//                            }
-//                        }
-//                        // регистрация
-//                        if (str.startsWith("/signup ")) {
-//                            String[] tokens = str.split(" ");
-//                            int result = AuthService.addUser(tokens[1], tokens[2], tokens[3]);
-//                            if (result > 0) {
-//                                sendMsg("Successful registration");
-//                            } else {
-//                                sendMsg("Registration failed");
-//                            }
-//                        }
-//                        // выход
-//                        if ("/end".equals(str)) {
-//                            isExit = true;
-//                            break;
-//                        }
-//                    }
+//            connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+//            statement = connection.createStatement();
+//        } catch (ClassNotFoundException | SQLException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//        String query = String.format("select '%s' from blacklist where nickFrom='%s'", nickTo, nickFrom);
+//        try {
+//            ResultSet rs = statement.executeQuery(query); // возвращает выборку через select
+//            int myHash = pass.hashCode();
 //
-//                    if (!isExit) {
-//                        while (true) {
-//                            String str = in.readUTF();
-//                            // для всех служебных команд и личных сообщений
-//                            if (str.startsWith("/") || str.startsWith("@")) {
-//                                if ("/end".equalsIgnoreCase(str)){
-//                                    // для оповещения клиента, т.к. без сервера клиент работать не должен
-//                                    out.writeUTF("/serverClosed");
-//                                    System.out.println("Client (" + socket.getInetAddress() + ") exited");
-//                                    break;
-//                                }
-//                                // вторая часть ДЗ. выполнение
-//                                if (str.startsWith("@")) {
-//                                    String[] tokens = str.split(" ", 2);
-//                                    server.sendPrivateMsg(this, tokens[0].substring(1), tokens[1]);
-//                                }
-//                                // черный список для пользователя. но пока что только в рамках одного запуска программы
-//                                if (str.startsWith("/blacklist ")) {
-//                                    BlackListDB.connect();
-//
-//
-//                                    String[] tokens = str.split(" ");
-//                                    blackList.add(tokens[1]);
-//                                    //BlackListDB.infoBlacklistBD(this.nickname, tokens[1]);
-//                                    //blacklistBD(this.nickname ,tokens[1]);
-//                                    sendMsg("You added " + tokens[1] + " to blacklist");
-//                                }//
-//                            } else {
-//                                server.broadcastMessage(this, nickname +": " + str);
-//                            }
-//                            System.out.println("Client (" + socket.getInetAddress() + "): " + str);
-//                        }
-//                    }
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                } finally {
-//                    try {
-//                        in.close();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                    try {
-//                        out.close();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                    try {
-//                        socket.close();
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }
-//                    server.unsubscribe(this);
+//            if (rs.next()) {
+//                String nick = rs.getString(1);
+//                int dbHash = rs.getInt(2);
+//                if (myHash == dbHash) {
+//                    return nick;
 //                }
-//            }).start();
-//        } catch (IOException e) {
+//            }
+//        } catch (SQLException e) {
 //            e.printStackTrace();
 //        }
 //    }
-//
-//    public void sendMsg(String msg) {
-//        try {
-//            out.writeUTF(msg);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//
-//    public String getNickname() {
-//        return nickname;
-//    }
-//
-//    public void setNickname(String nickname) {
-//        this.nickname = nickname;
-//    }
-//
-//    public boolean checkBlackList(String nickname) {
-//        return blackList.contains(nickname);
-//    }
-//}
+
+    // черный список у пользователя, а не у сервера
+    List<String> blackList;
+
+    public ClientHandler(ConsoleServer server, Socket socket) {
+        try {
+            this.server = server;
+            this.socket = socket;
+            this.in = new DataInputStream(socket.getInputStream());
+            this.out = new DataOutputStream(socket.getOutputStream());
+            this.blackList = new ArrayList<>();
+
+            new Thread(() -> {
+                boolean isExit = false;
+                try {
+                    while (true) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/auth")){
+                            String[] tokens = str.split(" ");
+                            String nick = AuthService.getNicknameByLoginAndPass(tokens[1], tokens[2]);
+                            if (nick != null) {
+                                if (!server.isNickBusy(nick)) {
+                                    sendMsg("/auth-OK");
+                                    setNickname(nick);
+                                    server.subscribe(ClientHandler.this);
+                                    break;
+                                } else {
+                                    sendMsg("Учетная запись уже используется");
+                                }
+                            } else {
+                                sendMsg("Неверный логин/пароль");
+                            }
+                        }
+                        // регистрация
+                        if (str.startsWith("/signup ")) {
+                            String[] tokens = str.split(" ");
+                            int result = AuthService.addUser(tokens[1], tokens[2], tokens[3]);
+                            if (result > 0) {
+                                sendMsg("Successful registration");
+                            } else {
+                                sendMsg("Registration failed");
+                            }
+                        }
+                        // выход
+                        if ("/end".equals(str)) {
+                            isExit = true;
+                            break;
+                        }
+                    }
+
+                    if (!isExit) {
+                        while (true) {
+                            String str = in.readUTF();
+                            // для всех служебных команд и личных сообщений
+                            if (str.startsWith("/") || str.startsWith("@")) {
+                                if ("/end".equalsIgnoreCase(str)){
+                                    // для оповещения клиента, т.к. без сервера клиент работать не должен
+                                    out.writeUTF("/serverClosed");
+                                    System.out.println("Client (" + socket.getInetAddress() + ") exited");
+                                    break;
+                                }
+                                // вторая часть ДЗ. выполнение
+                                if (str.startsWith("@")) {
+                                    String[] tokens = str.split(" ", 2);
+                                    server.sendPrivateMsg(this, tokens[0].substring(1), tokens[1]);
+                                }
+                                // черный список для пользователя. но пока что только в рамках одного запуска программы
+                                if (str.startsWith("/blacklist ")) {
+                                    BlackListDB.connect();
+
+
+                                    String[] tokens = str.split(" ");
+                                    blackList.add(tokens[1]);
+                                    //BlackListDB.infoBlacklistBD(this.nickname, tokens[1]);
+                                    //blacklistBD(this.nickname ,tokens[1]);
+                                    sendMsg("You added " + tokens[1] + " to blacklist");
+                                }//
+                            } else {
+                                server.broadcastMessage(this, nickname +": " + str);
+                            }
+                            System.out.println("Client (" + socket.getInetAddress() + "): " + str);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try {
+                        in.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        out.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    server.unsubscribe(this);
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(String msg) {
+        try {
+            out.writeUTF(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public boolean checkBlackList(String nickname) {
+        return blackList.contains(nickname);
+    }
+}
