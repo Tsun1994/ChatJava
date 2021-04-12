03e9d8b2a787214a7b4f2d2acfac76d568595f5c
Alexandr <tank006@yandex.ru>
2021-04-12 17:48:46 +0400

    finish

---
+++ src/sample/BlackListDB.java
@@ -0,0 +1,39 @@
+package sample;
+
+import java.sql.*;
+
+public class BlackListDB {
+    private static Connection connection;
+    private static Statement statement;
+
+    public static void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:main.db");
+            statement = connection.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String infoBlacklistBD ( String nickFrom) {
+        String query = String.format("select nickTo from blacklist where nickFrom='%s'",  nickFrom);
+        try {
+            ResultSet rs = statement.executeQuery(query);
+            if (rs.next()){
+                rs.getString("nickTo");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
